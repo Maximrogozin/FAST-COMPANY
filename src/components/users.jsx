8d3cfe8dcf071ab820120a996d9b2d61761450b5
Maxim Rogozin <maximrogozin@icloud.com>
2023-02-01 15:55:59 +0500

    create users file

---
+++ src/components/users.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import api from "../api";
+
+const Users = () => {
+  const [users, setUsers] = useState(api.users.fetchAll());
+
+  const renderQualities = (quality) => {
+    return quality.map((elem) => (
+      <span key={elem._id} className={"badge bg-" + elem.color}>
+        {elem.name}
+      </span>
+    ));
+  };
+  const handleDelete = (userId) => {
+    setUsers((prevState) => prevState.filter((users) => users._id !== userId));
+  };
+  const renderPhrase = () => {
+    if (users.length > 4 || users.length === 1) {
+      return (
+        <span className="badge bg-primary">
+          {users.length} человек тусанет с тобой сегодня
+        </span>
+      );
+    } else if (users.length > 1 && users.length < 5) {
+      return (
+        <span className="badge bg-primary">
+          {users.length} человека тусанет с тобой сегодня
+        </span>
+      );
+    } else if (users.length === 0) {
+      return <span className="badge bg-danger">Никто с тобой не тусанет</span>;
+    }
+  };
+
+  const renderUsers = () => {
+    return users.map((el) => (
+      <tr key={el._id}>
+        <td>{el.name}</td>
+        <td>{renderQualities(el.qualities)}</td>
+        <td>{el.profession.name}</td>
+        <td>{el.completedMeetings}</td>
+        <td>{el.rate} /5</td>
+        <td>
+          <button
+            className="btn btn-danger"
+            onClick={() => handleDelete(el._id)}
+          >
+            delete
+          </button>
+        </td>
+      </tr>
+    ));
+  };
+  return (
+    <>
+      <h2>{renderPhrase()}</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Имя</th>
+            <th>Качества</th>
+            <th>Профессия</th>
+            <th>Встретился, раз</th>
+            <th>Оценка</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>{renderUsers()}</tbody>
+      </table>
+    </>
+  );
+};
+
+export default Users;
